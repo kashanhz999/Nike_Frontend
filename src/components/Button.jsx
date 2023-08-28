5f194e57da8a99295df1535d775231c7404f4dc0
Kashan Haider <19cs162@jssaten.ac.in>
2023-08-28 22:44:37 +0530

    Added Super quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,13 @@
-const Button = ({label,iconUrl}) => {
+const Button = ({ label, iconUrl }) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 boprder font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
-        {label}
-        
-        <img
-            src={iconUrl}
-            alt="icon"
-            className='ml-2 rounded-full w-5 h-5'
-        />
-    </button>
-  )
-}
+    <button className="flex justify-center items-center gap-2 px-7 py-4 boprder font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+      {label}
 
-export default Button
+      {iconUrl && (
+        <img src={iconUrl} alt="icon" className="ml-2 rounded-full w-5 h-5" />
+      )}
+    </button>
+  );
+};
+
+export default Button;
